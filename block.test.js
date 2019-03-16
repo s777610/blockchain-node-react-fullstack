5aa5a792bbeac4637ac176cfa8e40ad1d8c59cb7
s777610 <s777610@gmail.com>
2019-03-16 16:22:32 -0700

    Implement unit test for minedBlock.hash matches difficulty level

--- block.test.js
+++ block.test.js
@@ -7,13 +7,24 @@ describe("Block", () => {
   const lastHash = "foo-hash";
   const hash = "bar-hash";
   const data = ["blockchain", "data"];
-  const block = new Block({ timestamp, lastHash, hash, data });
+  const nonce = 1;
+  const difficulty = 1;
+  const block = new Block({
+    timestamp,
+    lastHash,
+    hash,
+    data,
+    nonce,
+    difficulty
+  });
 
   it("hash a tiemstamp, lastHash, hash, and data property", () => {
     expect(block.timestamp).toEqual(timestamp);
     expect(block.lastHash).toEqual(lastHash);
     expect(block.hash).toEqual(hash);
     expect(block.data).toEqual(data);
+    expect(block.nonce).toEqual(nonce);
+    expect(block.difficulty).toEqual(difficulty);
   });
 
   describe("genesis()", () => {
@@ -51,7 +62,19 @@ describe("Block", () => {
 
     it("creates a SHA-256 `hash` based on the proper inputs", () => {
       expect(minedBlock.hash).toEqual(
-        cryptoHash(minedBlock.timestamp, lastBlock.hash, data)
+        cryptoHash(
+          minedBlock.timestamp,
+          minedBlock.nonce,
+          minedBlock.difficulty,
+          lastBlock.hash,
+          data
+        )
+      );
+    });
+
+    it("set a `hash` that matches the difficulty criteria", () => {
+      expect(minedBlock.hash.substring(0, minedBlock.difficulty)).toEqual(
+        "0".repeat(minedBlock.difficulty)
       );
     });
   });

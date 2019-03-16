0269859f1a73659bc40f0097b3e79b2df0e74b51
s777610 <s777610@gmail.com>
2019-03-16 00:04:45 -0700

    create isValidChain()

--- blockchain.js
+++ blockchain.js
@@ -1,8 +1,27 @@
 const Block = require("./block");
+const cryptoHash = require("./crypto-hash");
 
 class Blockchain {
   constructor() {
     this.chain = [Block.genesis()];
+  }
+
+  static isValidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false;
+    }
+
+    for (let i = 1; i < chain.length; i++) {
+      const block = chain[i];
+      const actualLastHash = chain[i - 1].hash;
+      const { timestamp, lastHash, data, hash } = block;
+      const validatedHash = cryptoHash(timestamp, lastHash, data);
+
+      if (lastHash !== actualLastHash) return false;
+      if (validatedHash !== hash) return false;
+    }
+
+    return true;
   }
 
   addBlock({ data }) {

5d143c0156da0b4fe4ef63918282585bc8944638
s777610 <s777610@gmail.com>
2019-03-20 20:52:49 -0700

    generate publicKey

---
+++ wallet/index.js
@@ -0,0 +1,14 @@
+const { STARTING_BALANCE } = require("../config");
+const { ec } = require("../util");
+
+class Wallet {
+  constructor() {
+    this.balance = STARTING_BALANCE;
+
+    const KeyPair = ec.genKeyPair();
+
+    this.publicKey = KeyPair.getPublic().encode("hex");
+  }
+}
+
+module.exports = Wallet;

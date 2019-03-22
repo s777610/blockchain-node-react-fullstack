7002b4c0011df0c83c04911e7c0e322c2c11b38f
s777610 <s777610@gmail.com>
2019-03-21 19:52:55 -0700

    add test suite for createTransaction method

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,5 +1,6 @@
 const Wallet = require("./index");
 const { verifySignature } = require("../util");
+const Transaction = require("./transaction");
 
 describe("Wallet", () => {
   let wallet;
@@ -37,6 +38,41 @@ describe("Wallet", () => {
           signature: new Wallet().sign(data)
         })
       ).toBe(false);
+    });
+  });
+
+  describe("createTransaction()", () => {
+    describe("and the amount exceeds the balance", () => {
+      it("throws an error", () => {
+        expect(() =>
+          wallet.createTransaction({
+            amount: 999999,
+            recipient: "recipient-public-key"
+          })
+        ).toThrow("Amount exceeds balance");
+      });
+    });
+
+    describe("and the amount is valid", () => {
+      let transaction, amount, recipient;
+
+      beforeEach(() => {
+        amount = 50;
+        recipient = "foo-recipient-public-key";
+        transaction = wallet.createTransaction({ amount, recipient });
+      });
+
+      it("creates an instance of `Transaction`", () => {
+        expect(transaction instanceof Transaction).toBe(true);
+      });
+
+      it("matches the transaction input with the wallet", () => {
+        expect(transaction.input.address).toEqual(wallet.publicKey);
+      });
+
+      it("outputs the amount the recipient", () => {
+        expect(transaction.outputMap[recipient]).toEqual(amount);
+      });
     });
   });
 });

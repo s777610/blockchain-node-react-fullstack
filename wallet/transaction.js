55b31e580eee9a21b838c68c6714ae5860e921d5
s777610 <s777610@gmail.com>
2019-03-21 22:39:03 -0700

    be able to send coin to same user

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -40,7 +40,16 @@ class Transaction {
   }
 
   update({ senderWallet, recipient, amount }) {
-    this.outputMap[recipient] = amount;
+    if (amount > this.outputMap[senderWallet.publicKey]) {
+      throw new Error("Amount exceeds balance");
+    }
+
+    if (!this.outputMap[recipient]) {
+      this.outputMap[recipient] = amount;
+    } else {
+      this.outputMap[recipient] += amount;
+    }
+
     this.outputMap[senderWallet.publicKey] -= amount;
     this.input = this.createInput({ senderWallet, outputMap: this.outputMap });
   }

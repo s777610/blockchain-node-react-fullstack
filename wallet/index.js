1a37ba2ddb06a91e5a72cb92c1a381f621ba9921
s777610 <s777610@gmail.com>
2019-03-26 14:44:04 -0700

    implement test for calculateBalance

--- wallet/index.js
+++ wallet/index.js
@@ -23,7 +23,14 @@ class Wallet {
     return this.keyPair.sign(cryptoHash(data)); // return a Signature
   }
 
-  createTransaction({ amount, recipient }) {
+  createTransaction({ amount, recipient, chain }) {
+    if (chain) {
+      this.balance = Wallet.calculateBalance({
+        chain,
+        address: this.publicKey
+      });
+    }
+
     if (amount > this.balance) {
       throw new Error("Amount exceeds balance");
     }
@@ -33,6 +40,24 @@ class Wallet {
       recipient,
       amount
     });
+  }
+
+  static calculateBalance({ chain, address }) {
+    let outputsTotal = 0;
+
+    for (let i = 1; i < chain.length; i++) {
+      const block = chain[i];
+
+      for (let transaction of block.data) {
+        const addressOutput = transaction.outputMap[address];
+
+        if (addressOutput) {
+          outputsTotal = outputsTotal + addressOutput;
+        }
+      }
+    }
+
+    return STARTING_BALANCE + outputsTotal;
   }
 }
 
--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,6 +1,8 @@
 const Wallet = require("./index");
 const { verifySignature } = require("../util");
 const Transaction = require("./transaction");
+const Blockchain = require("../blockchain");
+const { STARTING_BALANCE } = require("../config");
 
 describe("Wallet", () => {
   let wallet;
@@ -72,6 +74,98 @@ describe("Wallet", () => {
 
       it("outputs the amount the recipient", () => {
         expect(transaction.outputMap[recipient]).toEqual(amount);
+      });
+    });
+
+    describe("and a chain is passed", () => {
+      it("calls `Wallet.calculateBalance`", () => {
+        const calculateBalanceMock = jest.fn();
+
+        const originalCalculateBalance = Wallet.calculateBalance;
+
+        Wallet.calculateBalance = calculateBalanceMock;
+
+        wallet.createTransaction({
+          recipient: "foo",
+          amount: 10,
+          chain: new Blockchain().chain
+        });
+
+        expect(calculateBalanceMock).toHaveBeenCalled();
+
+        Wallet.calculateBalance = originalCalculateBalance;
+      });
+    });
+  });
+
+  describe("calculateBalance()", () => {
+    let blockchain;
+
+    beforeEach(() => {
+      blockchain = new Blockchain();
+    });
+
+    describe("and there are no outputs for the wallet", () => {
+      it("returns the `STARTING_BALANCE`", () => {
+        expect(
+          Wallet.calculateBalance({
+            chain: blockchain.chain,
+            address: wallet.publicKey
+          })
+        ).toEqual(STARTING_BALANCE);
+      });
+    });
+
+    describe("and there are outputs for the wallet", () => {
+      let transactionOne, transactionTwo;
+
+      beforeEach(() => {
+        transactionOne = new Wallet().createTransaction({
+          recipient: wallet.publicKey,
+          amount: 50
+        });
+
+        transactionTwo = new Wallet().createTransaction({
+          recipient: wallet.publicKey,
+          amount: 60
+        });
+
+        blockchain.addBlock({ data: [transactionOne, transactionTwo] });
+      });
+
+      it("adds the sum of all outputs to the wallet balance", () => {
+        expect(
+          Wallet.calculateBalance({
+            chain: blockchain.chain,
+            address: wallet.publicKey
+          })
+        ).toEqual(
+          STARTING_BALANCE +
+            transactionOne.outputMap[wallet.publicKey] +
+            transactionTwo.outputMap[wallet.publicKey]
+        );
+      });
+
+      describe("and the wallet has made a transaction", () => {
+        let recentTransaction;
+
+        beforeEach(() => {
+          recentTransaction = wallet.createTransaction({
+            recipient: "foo-address",
+            amount: 30
+          });
+
+          blockchain.addBlock({ data: [recentTransaction] });
+        });
+
+        it("returns the output amount of the recent transaction", () => {
+          expect(
+            Wallet.calculateBalance({
+              chain: blockchain.chain,
+              address: wallet.publicKey
+            })
+          ).toEqual(recentTransaction.outputMap[wallet.publicKey]);
+        });
       });
     });
   });

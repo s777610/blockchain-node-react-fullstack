a99d6a68a9d2461b70a1298253a16e784d9362dd
s777610 <s777610@gmail.com>
2019-03-27 01:06:58 -0700

    implement couple tests for validate transactions

--- index.js
+++ index.js
@@ -75,6 +75,17 @@ app.get("/api/mine-transactions", (req, res) => {
   res.redirect("/api/blocks");
 });
 
+app.get("/api/wallet-info", (req, res) => {
+  const address = wallet.publicKey;
+  res.json({
+    address,
+    balance: Wallet.calculateBalance({
+      chain: blockchain.chain,
+      address
+    })
+  });
+});
+
 const syncWithRootState = () => {
   request(
     { url: `${ROOT_NODE_ADDRESS}/api/blocks` },

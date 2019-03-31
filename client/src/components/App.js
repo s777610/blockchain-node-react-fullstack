53434e60347a7b859e9926c81512f4b6e8a700ef
s777610 <s777610@gmail.com>
2019-03-30 23:26:26 -0700

    display address and balabce on screen

---
+++ client/src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = { walletInfo: { address: "fooxv6", balance: 9999 } };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/api/wallet-info")
+      .then(response => response.json())
+      .then(json => this.setState({ walletInfo: json }));
+  }
+
+  render() {
+    const { address, balance } = this.state.walletInfo;
+
+    return (
+      <div>
+        <div>Welcome to the blockchain...</div>
+        <div>Address: {address}</div>
+        <div>Balance: {balance}</div>
+      </div>
+    );
+  }
+}
+
+export default App;

b9cf4ed2fe98efbf01db52943eb55fd62ef1e0e2
s777610 <s777610@gmail.com>
2019-04-05 19:21:27 -0700

    add route handler for pagination

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -45620,7 +45620,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60469" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65468" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
